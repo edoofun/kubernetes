1c9e41e88405266560eba144b75b99322689a695
edoofun <99808032+edoofun@users.noreply.github.com>
2024-09-13 18:04:24 +0800

    Update base.go

--- staging/src/k8s.io/client-go/pkg/version/base.go
+++ staging/src/k8s.io/client-go/pkg/version/base.go
@@ -56,7 +56,7 @@ var (
 	// NOTE: The $Format strings are replaced during 'git archive' thanks to the
 	// companion .gitattributes file containing 'export-subst' in this same
 	// directory.  See also https://git-scm.com/docs/gitattributes
-	gitVersion   string = "v1.27.16"
+	gitVersion   = "v0.0.0-master+$Format:%H$"
 	gitCommit    string = "$Format:%H$" // sha1 from git, output of $(git rev-parse HEAD)
 	gitTreeState string = ""            // state of git tree, either "clean" or "dirty"
 
